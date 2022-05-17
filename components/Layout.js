d86c708107863e8666ff350ef72280d6524f0d6a
harrishalisha <noranalisha@gmail.com>
2022-05-17 20:52:37 +0100

    Layout

---
+++ components/Layout.js
@@ -0,0 +1,16 @@
+import styles from '../styles/Layout.module.css'
+
+const Layout = ({children}) => {
+  return (
+    <div className={styles.container}>
+        <main className={styles.main}>
+            <h1>Hello</h1>
+             {children}
+        </main>
+    </div>
+  )
+}
+
+export default Layout
+
+//children is Components props from api/_app
