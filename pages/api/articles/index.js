30ba8da819145ab5df1169aca48b8bd7fa9c4156
harrishalisha <noranalisha@gmail.com>
2022-05-20 07:29:10 +0100

    get data from api

--- pages/api/articles/index.js
+++ pages/api/articles/index.js
@@ -1,5 +1,6 @@
 import { articles} from '../../../data'
 
+/* /api/articles */
 export default function handler(req, res) {
     res.status(200).json(articles)
   }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import { Fragment } from 'react';
 import ArticleList from '../components/ArticleList';
+import { server } from '../config';
 import styles from '../styles/Home.module.css';
 
 
 export const getStaticProps = async() => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const res = await fetch(`${server}/api/articles`)
   const articles = await res.json()
   
   return {
